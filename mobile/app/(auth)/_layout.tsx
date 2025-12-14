d8933b6526d1d3274a7905311d6ef5b0cf1afc07
shreeram312 <126177107+shreeram312@users.noreply.github.com>
2025-12-14 22:34:32 +0530

    chore:change eas.josn file

--- mobile/app/(auth)/_layout.tsx
+++ mobile/app/(auth)/_layout.tsx
@@ -5,7 +5,7 @@ export default function AuthRoutesLayout() {
   const { isSignedIn } = useAuth();
 
   if (isSignedIn) {
-    return <Redirect href={"/"} />;
+    return <Redirect href={"/(auth)/index"} />;
   }
 
   return <Stack screenOptions={{ headerShown: false }} />;
