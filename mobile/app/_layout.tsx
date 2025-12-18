27b1c6a8870ee8881fbe41d179341299450c4af6
shreeram312 <126177107+shreeram312@users.noreply.github.com>
2025-12-18 10:47:32 +0530

    feat: add splash-screen & change logo

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,20 +1,31 @@
-import { ClerkProvider } from "@clerk/clerk-expo";
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  ClerkProvider,
+  useAuth,
+} from "@clerk/clerk-expo";
 import { Slot } from "expo-router";
 import { tokenCache } from "@clerk/clerk-expo/token-cache";
 import SafeScreen from "@/components/safe-screen";
+import * as SplashScreen from "expo-splash-screen";
+
+SplashScreen.preventAutoHideAsync();
+
+const clerkPublishableKey = process.env
+  .EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY as string;
+
+if (!clerkPublishableKey) {
+  throw new Error("Missing Clerk publishable key");
+}
 
 export default function RootLayout() {
-  const clerkPublishableKey = process.env
-    .EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY as string;
-
-  if (!clerkPublishableKey) {
-    throw new Error("Missing Clerk publishable key");
-  }
   return (
     <ClerkProvider tokenCache={tokenCache} publishableKey={clerkPublishableKey}>
-      <SafeScreen>
-        <Slot />
-      </SafeScreen>
+      <ClerkLoaded>
+        <SafeScreen>
+          <Slot />
+        </SafeScreen>
+      </ClerkLoaded>
     </ClerkProvider>
   );
 }
