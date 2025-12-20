41d186d862952a4f8f9602f1cf74a714f4a4066d
shreeram312 <126177107+shreeram312@users.noreply.github.com>
2025-12-20 16:00:34 +0530

    feat: add search screen

--- mobile/app/(tabs)/search.tsx
+++ mobile/app/(tabs)/search.tsx
@@ -1,10 +1,63 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, TextInput, FlatList } from "react-native";
+import React, { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 
+import { useDebounce } from "@/hooks/use-debounce";
+import { MEAL_API } from "@/services/meal-api";
+import { searchStyles } from "@/assets/styles/search-styles";
+import { Ionicons } from "@expo/vector-icons";
+import RecipeCard from "@/components/recipe-card";
 const SearchScreen = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [recipes, setRecipes] = useState([]);
+  const debouncedSearchQuery = useDebounce(searchQuery, 500);
+
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["search-recipes", debouncedSearchQuery],
+    // queryFn: async () => {
+    //   const res = await MEAL_API.get(
+    //     `/recipes/complexSearch?query=${debouncedSearchQuery}`
+    //   );
+    // },
+  });
+
   return (
-    <View>
-      <Text>SearchScreen</Text>
+    <View style={searchStyles.container}>
+      <View style={searchStyles.searchSection}>
+        <View style={searchStyles.searchContainer}>
+          <Ionicons name="search" size={24} color="black" />
+          <TextInput
+            style={searchStyles.searchInput}
+            placeholder="Search for a recipe"
+            value={searchQuery}
+            onChangeText={setSearchQuery}
+          />
+
+          <View style={searchStyles.resultsSection}>
+            <View style={searchStyles.resultsHeader}>
+              <Text style={searchStyles.resultsTitle}>
+                {searchQuery
+                  ? `Results for "${searchQuery}"`
+                  : "Search for a recipe"}
+              </Text>
+              <Text style={searchStyles.resultsCount}>
+                {recipes.length} results
+              </Text>
+            </View>
+          </View>
+
+          {isLoading ? (
+            <View style={searchStyles.loadingContainer}>
+              <Text>Loading...</Text>
+            </View>
+          ) : (
+            <FlatList
+              data={recipes}
+              renderItem={({ item }) => <RecipeCard recipe={item} />}
+            />
+          )}
+        </View>
+      </View>
     </View>
   );
 };
