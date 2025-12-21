32eab6ccd5c88bcd769550b314988c383581c16e
shreeram312 <126177107+shreeram312@users.noreply.github.com>
2025-12-21 12:52:07 +0530

    feat: create favourite screen with data fetching from custom backend

--- mobile/app/(tabs)/favourites.tsx
+++ mobile/app/(tabs)/favourites.tsx
@@ -1,7 +1,29 @@
 import { View, Text } from "react-native";
-import React from "react";
+import React, { useState } from "react";
+import { useAuth, useUser } from "@clerk/clerk-expo";
+import { useQuery } from "@tanstack/react-query";
 
 const FavouritesScreen = () => {
+  const { user } = useUser();
+
+  const {
+    data: favouriteRecipes,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ["favourites", user?.id],
+    queryFn: async () => {
+      console.log(user?.id);
+      const response = await fetch(
+        `${process.env.EXPO_PUBLIC_BACKEND_API_URL}/api/favourites/${user?.id}`
+      );
+      const data = await response.json();
+      return data;
+    },
+  });
+
+  console.log(favouriteRecipes);
+
   return (
     <View>
       <Text>FavouritesScreen</Text>
---
+++ mobile/services/api.ts
@@ -0,0 +1,1 @@
+export const API_URL = "https://reciperadar-4p49.onrender.com";
